4480c4b86a7b6e0bfdf8afc58ed319d1f83532d1
Rononoa13 <sumitluitel42@gmail.com>
2022-11-19 21:08:49 +0545

    first commit for python practices

---
+++ Thinking-In-Types/02_Type_System/variables.py
@@ -0,0 +1,16 @@
+# -------------------------------------------------------------------------
+# Louis is going for a party, he has got baloons, crackers and hats.
+# -------------------------------------------------------------------------
+
+
+#Declaring a new variable called as 'box'
+box = 'balloons'
+print(box)
+
+# Lous changes his mind, he now wants to pack crackers instead.
+box = 'crackers'
+print(box)
+
+# Lous changes his mind, he now wants to pack hats instead.
+box = 'hats'
+print(box)
